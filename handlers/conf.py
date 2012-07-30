7db37fb51506aca326760b690580ea16dd1f7b98
Benoît Baliguet <benoit.baliguet@gmail.com>
2012-07-30 23:39:18 +0200

    working on REST handler

--- handlers/conf.py
+++ handlers/conf.py
@@ -1,4 +1,4 @@
-import webapp2, jinja2, os
+import webapp2, jinja2, os, logging
 from google.appengine.api import users
 from livetrack import models
 
@@ -10,28 +10,48 @@ def check_user(handler):
 	if user:
 		return True
 	else:	
-		self.redirect(users.create_login_url(self.request.uri))
+		self.response.status = 403
 		return False
 
 class ConfHandler(webapp2.RequestHandler):
 	def get(self):
-		if check_user(self):
+		user = users.get_current_user()
+		uri = self.request.uri
+		if user:
 			template = jinja_environment.get_template('templates/conf.html')
 			self.response.out.write(template.render({
+					'logout_url' : users.create_logout_url(uri), 
 					'trackings' : models.Tracking.all()
 				}))
+		else:	
+			self.redirect(users.create_login_url(uri))
+			
 	
 	def post(self):
 		if check_user(self):
 			newTracking = models.Tracking(url = self.request.get("url"))
 			newTracking.put()
+			self.response.out.write(newTracking.key())
 
 	def delete(self):
 		if check_user(self):
 			id = self.request.get("id")
-			tracking = models.Tracking.get_by_id(id)
+			tracking = models.Tracking.get_by_key_name(id)
 			if tracking != None:
+				logging.debug(tracking)
 				tracking.delete()
+			else:
+				self.response.status = 400
+
+	def put(self):
+		if check_user(self):
+			id = self.request.get("id")
+			print id
+			tracking = models.Tracking.get_by_key_name(id)
+			if tracking != None:
+				tracking.active = not tracking.active
+				self.response.out.write("true" if tracking.active else "false")
+				tracking.put()
 			else:
 				self.response.status = 400
 
--- static/conf.js
+++ static/conf.js
@@ -1,3 +1,79 @@
 $(function () {
-	alert("test");
+	var add = function(event) {
+		var input =  $("#addUrl"), url = input.val(), element
+		if (url) {
+			// fake add it. If it fails ... well it fails !
+			element = $(document.createElement("div")).addClass("tracking").html(url)
+			$("#configuration").append(element)
+			$.ajax({
+				type: 'POST',
+				url: '/conf',
+				data: { url: url },
+				success: function(data){
+					element.attr("id", data);
+					element = null;
+				},
+				error: function(xhr, type){
+					element.addClass("failed")
+					element = null
+				}
+			})
+		} else {
+			input.addClass("error")
+		}
+		event.preventDefault()
+	},
+
+	remove = function (event) {
+		if (window.confirm("Do you reeeeeally want to delete this?")) {
+			var element =  $(event.target), 
+				id = element.attr("id")
+			if (id) {
+				$.ajax({
+					type: 'DELETE',
+					url: '/conf',
+					data: { id: id },
+					success: function(data){
+						element.remove();
+						element = null;
+					},
+					error: function(xhr, type){
+						element.addClass("failed")
+						element = null
+					}
+				})
+			}
+		}
+	},
+
+	toggleActive = function (event) {
+		var element =  $(event.target), 
+			id = element.attr("id")
+		if (id) {
+			$.ajax({
+				type: 'PUT',
+				url: '/conf',
+				data: { id: id },
+				success: function(data){
+					element.toggleClass("active")
+					element = null;
+				},
+				error: function(xhr, type){
+					element.addClass("failed")
+					element = null
+				}
+			})
+		}
+	}
+
+	$("#adUrlBtn").on({
+		click: add,
+		singleTap : add
+	})
+	$(".tracking").on({
+		click : toggleActive,
+		singleTap : toggleActive,
+		dblclick : remove,
+		longTap : remove
+	})
 })
