50b40d5fc2afb388d86472f625783c23440bf4f0
Benoît Baliguet <benoit.baliguet@gmail.com>
2012-08-01 10:08:19 +0200

    add samples for testing

--- test_real.py
+++ test_real.py
@@ -1,10 +1,17 @@
-from livetrack.extract import extract_pos
+import urllib
+from livetrack.extract import extract_kml
 from xml.etree.ElementTree import ElementTree
 from xml.etree.ElementTree import XMLParser
 
 
 def extract():
-	extract_pos("http://www.sat-view.fr/comptes/celtikup/traces/")
+	links = extract_kml("http://www.sat-view.fr/comptes/celtikup/traces/")
+	for link in links:
+		data = urllib.urlopen(link)
+		target = "test/traces/" + link[47:len(link)]
+		print target
+		f = open(target, "w")
+		f.write(data.read())
 
 if __name__ == '__main__':
 	extract()
